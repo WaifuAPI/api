8fbd2cb3423987c9b9472ca4b9c2aeb83fa0ea53
Kyoyo <hey.kyoyo@gmail.com>
2021-09-24 02:15:03 +0530

    Fixed a small bug again

--- src/app.js
+++ src/app.js
@@ -30,29 +30,29 @@ if (process.env.LOGGER === 'true') {
   //   maxFiles: '14d',
   // })
 
-  app
-    .use
-    /* Winson logger */
+  // app
+  //   .use
+  //   /* Winson logger */
 
-    // expressWinston.logger({
-    //   transports: [transport],
-    //   format: winston.format.combine(
-    //     winston.format.colorize(),
-    //     winston.format.json()
-    //   ),
-    //   meta: true,
-    //   msg(req, res) {
-    //     return `${res.statusCode} - ${req.method} - ${
-    //       res.responseTime
-    //     }ms - IP: ${req.ip} IPs: ${requestIp.getClientIp(req)}`
-    //   },
-    //   expressFormat: true,
-    //   colorize: false,
-    //   ignoreRoute(req, res) {
-    //     return false
-    //   },
-    // })
-    ()
+  //   // expressWinston.logger({
+  //   //   transports: [transport],
+  //   //   format: winston.format.combine(
+  //   //     winston.format.colorize(),
+  //   //     winston.format.json()
+  //   //   ),
+  //   //   meta: true,
+  //   //   msg(req, res) {
+  //   //     return `${res.statusCode} - ${req.method} - ${
+  //   //       res.responseTime
+  //   //     }ms - IP: ${req.ip} IPs: ${requestIp.getClientIp(req)}`
+  //   //   },
+  //   //   expressFormat: true,
+  //   //   colorize: false,
+  //   //   ignoreRoute(req, res) {
+  //   //     return false
+  //   //   },
+  //   // })
+  //   ()
   app.use(ipLogger)
 }
 
--- src/handlers/logger/ip.js
+++ src/handlers/logger/ip.js
@@ -3,11 +3,9 @@ const requestIp = require('request-ip')
 
 // Logger In One File
 module.exports.ipLogger = function ipLogger(req, res, next) {
-  const log = `${new Date()}  - STATUS=${res.statusCode} - METHOD=${
+  const log = `${new Date()} - STATUS=${res.statusCode} - METHOD=${
     req.method
-  } - IP=${req.ip} | ${requestIp.getClientIp(req)} - URL=${
-    req.originalUrl
-  } - RES_TIME: ${res.responseTime}ms\n`
+  } - IP=${req.ip} | ${requestIp.getClientIp(req)} - URL=${req.originalUrl}\n`
 
   // fs.appendFile('./logs/ip-logs.log', log, err => {
   //   if (err) throw err
