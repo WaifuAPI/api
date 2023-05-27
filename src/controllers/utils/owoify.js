487b0f22cb4d7d9c469db1bc40e941af493d7a6f
kyrea <notaeryk@gmail.com>
2023-05-27 22:48:00 +0530

    Added console logger

--- src/controllers/utils/owoify.js
+++ src/controllers/utils/owoify.js
@@ -1,4 +1,5 @@
 const createError = require('http-errors')
+const requestIp = require('request-ip')
 const owoify = require('owoify-js').default
 const Stats = require('../../models/schemas/Stat')
 
@@ -12,6 +13,11 @@ module.exports = async function getOwofiyText(req, res, next) {
     res.status(200).json({
       text: owoify(text),
     })
+    console.log(
+      `${req.method} | ${moment(Date.now()).format()} ${requestIp.getClientIp(
+        req
+      )} to ${req.path} - ${JSON.stringify(req.query)}`
+    )
     return await Stats.findOneAndUpdate (
       { _id: "systemstats" },
       { $inc: { owoify: 1 } },
