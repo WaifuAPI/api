02fd9acf66f1606b9e906796528d648e5a0eb7dd
kyrea <notaeryk@gmail.com>
2023-12-28 01:21:56 +0530

    Renamed files and removed extra spaces

---
+++ src/controllers/v4/interactions/cry.js
@@ -0,0 +1,27 @@
+import createError from 'http-errors';
+import Cry from '../../../models/schemas/Cry.js';
+import Stats from '../../../models/schemas/Stat.js';
+
+// Get random Anime Cry
+const getRandomCry = async (req, res, next) => {
+  try {
+    const [result] = await Cry.aggregate([
+      // Select a random document from the results
+      { $sample: { size: 1 } },
+      { $project: { __v: 0, _id: 0 } },
+    ]);
+
+    if (!result) {
+      return next(createError(404, 'Could not find any Cry Gif'));
+    }
+
+    res.status(200).json(result);
+
+    await Stats.findOneAndUpdate({ _id: 'systemstats' }, { $inc: { cry: 1 } });
+  } catch (error) {
+    await Stats.findOneAndUpdate({ _id: 'systemstats' }, { $inc: { failed_requests: 1 } });
+    return next(error);
+  }
+};
+
+export default getRandomCry;
---
+++ src/controllers/v4/interactions/cuddle.js
@@ -0,0 +1,27 @@
+import createError from 'http-errors';
+import Cuddle from '../../../models/schemas/Cuddle.js';
+import Stats from '../../../models/schemas/Stat.js';
+
+// Get random Anime Cuddle
+const getRandomCuddle = async (req, res, next) => {
+  try {
+    const [result] = await Cuddle.aggregate([
+      // Select a random document from the results
+      { $sample: { size: 1 } },
+      { $project: { __v: 0, _id: 0 } },
+    ]);
+
+    if (!result) {
+      return next(createError(404, 'Could not find any Cuddle Gif'));
+    }
+
+    res.status(200).json(result);
+
+    await Stats.findOneAndUpdate({ _id: 'systemstats' }, { $inc: { cuddle: 1 } });
+  } catch (error) {
+    await Stats.findOneAndUpdate({ _id: 'systemstats' }, { $inc: { failed_requests: 1 } });
+    return next(error);
+  }
+};
+
+export default getRandomCuddle;
