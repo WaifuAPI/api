3692935ad81da0a164331586fa4929656ad817b6
kyrea <notaeryk@gmail.com>
2023-12-27 16:51:56 +0530

    Added rateLimit to fact endpoints

--- src/routes/v4/textUtilities/facts.js
+++ src/routes/v4/textUtilities/facts.js
@@ -1,6 +1,7 @@
 // routes/index.js
 import { Router } from 'express';
 import getRandomFact from '../../../controllers/v3/facts/randomFacts.js';
+import createRateLimiter from '../../../middlewares/rateLimit.js';
 import authorize from '../../../middlewares/authorize.js';
 import incrementData from '../../../modules/database/add.js';
 
@@ -22,8 +23,15 @@ router
    *
    * @apiError (Unauthorized 401) Unauthorized Only authenticated users can access the data.
    * @apiError (Forbidden 403) Forbidden Only users can access the data.
+   * @apiError (Too Many Requests 429) TooManyRequests The client has exceeded the allowed number of requests within the time window.
+   * @apiError (Internal Server Error 500) InternalServerError An error occurred while processing the rate limit.
+   *
+   * @function createRateLimit
+   * @description Creates a rate limiter middleware to control the frequency of requests.
+   * @returns {function} Express middleware function that handles rate limiting.
+   *
    */
-  .get(authorize(config.roles.USER), getRandomFact)
+  .get(authorize(config.roles.USER), createRateLimiter(), getRandomFact)
   /**
    * @api {post} v4/fact Increment Fact Data
    * @apiDescription Increment data related to facts (only accessible by admins).
@@ -37,9 +45,16 @@ router
    * @apiSuccess {Object} result Result of the data incrementation.
    *
    * @apiError (Unauthorized 401) Unauthorized Only authenticated admins can access the data.
-   * @apiError (Forbidden 403) Forbidden Only admins can access the data.
+   * @apiError (Forbidden 403) Forbidden Only users can access the data.
+   * @apiError (Too Many Requests 429) TooManyRequests The client has exceeded the allowed number of requests within the time window.
+   * @apiError (Internal Server Error 500) InternalServerError An error occurred while processing the rate limit.
+   *
+   * @function createRateLimit
+   * @description Creates a rate limiter middleware to control the frequency of requests.
+   * @returns {function} Express middleware function that handles rate limiting.
+   *
    */
-  .post(authorize(config.roles.ADMIN), incrementData('Fact'));
+  .post(authorize(config.roles.ADMIN), createRateLimiter(), incrementData('Fact'));
 
 // Export the router
 export default router;
