f348b0dbce19b0834b6a0e90c2a658af98b10198
kyrea <notaeryk@gmail.com>
2023-12-27 18:03:47 +0530

    Improved routes index.js code with better documentation

--- src/routes/v4/index.js
+++ src/routes/v4/index.js
@@ -1,11 +1,31 @@
+// routes/index.js
 import { Router } from 'express';
-import factRoutes from './textUtilities/facts.js';
+import factRoutes from './textUtilities/fact.js';
 
-const router = Router();
 /**
- * GET v1/docs
+ * Express Router for handling API routes.
+ * @type {Router}
+ * @namespace routes
+ */
+const router = Router();
+
+/**
+ * @api {use} v1/fact Use Fact Routes
+ * @apiDescription Mount the fact-related routes for handling text utilities.
+ * @apiName UseFactRoutes
+ * @apiGroup Routes
+ *
+ * @apiSuccess {Object} routes Fact-related routes mounted on the parent router.
+ *
+ * @function createFactRoutes
+ * @description Creates and returns a set of routes for handling text utilities related to facts.
+ * @returns {Object} Fact-related routes.
  */
 
 router.use('/fact', factRoutes);
 
+/**
+ * Exporting the router for use in other parts of the application.
+ * @exports {Router} router - Express Router instance with mounted routes.
+ */
 export default router;
